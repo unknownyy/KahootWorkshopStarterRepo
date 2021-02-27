4336b7af396df25e37db2b98374ab2335fb13024
you you <cyouyiun@gmail.com>
2021-02-27 02:53:52 +0000

    alot

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Kahoot Workshop Starter Repo
+const Kahoot = require("kahoot.js-updated");
+const client = new Kahoot();
+
+client.join('04182685', Math.random() + "- You");
+
+client.on("QuizStart", () => {
+  console.log("The quiz has started!");
+});
+
+client.on("QuizEnd", () => {
+  console.log("The quiz has ended.");
+});
+
+client.on("QuestionStart", question => {
+  console.log("New question!");
+  console.log(question.question);
+  console.log("Answering Option A, I don't even know what it is!");
+  question.answer(0);
+});
+
+function verifyProperty(array, property) {
+  let finalNo
+  array.forEach(function(value, i) {
+    if (value[property] === true) {finalNo = i}
+  });
+  return(finalNo)
+}
